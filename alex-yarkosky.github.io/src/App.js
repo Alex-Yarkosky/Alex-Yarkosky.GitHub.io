5f9905c0dd6960c4c06b647a91262b3c767f1ef5
Alex Yarkosky <yarkoskya@allegheny.edu>
2021-08-25 22:19:57 -0400

    begin adding react app

---
+++ alex-yarkosky.github.io/src/App.js
@@ -0,0 +1,48 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  <div>
+    <h1>
+      Alexander Yarkosky
+    </h1>
+    <p>
+      Welcome to my personal website! Check out some my projects or my social media accounts!
+    </p>
+    <p>
+      As for myself, I have been programming since I started high school and recently graduated from Allegheny College, double majoring in Computer Science and Political Science.
+      I love how coding can both provide enjoyment and challenge, creating a desirable mix that helps drive my passion.
+      Looking forward, I am excited to continue my knowledge in all areas of software development, particularly learning new languages and tools to use.
+      Feel free to contribute to my repositories or post issues for any bugs you encounter or features you would like to see!
+    </p>
+    <h1>
+      Recent Projects
+    </h1>
+    <h3>
+      Reapportionment App/Undergrad Thesis
+    </h3>
+    <h3>
+      Redistricted Maps
+    </h3>
+  </div>
+  );
+}
+
+export default App;
